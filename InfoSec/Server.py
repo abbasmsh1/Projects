e91a646f52ce6191e1ba1358a1ca3115b3aa6a1b
Abbas Mustafa <52071783+abbasmsh1@users.noreply.github.com>
2024-04-29 14:57:14 +0500

    Update Server.py

--- InfoSec/Server.py
+++ InfoSec/Server.py
@@ -5,19 +5,25 @@ import numpy as np
 
 # Hook PyTorch to PySyft
 hook = sy.TorchHook(torch)
+
 # Define server class
 class FederatedServer:
     def __init__(self, model, clients):
         self.model = model
+        self.clients = clients
 
-    def aggregate_weights(self, *weights):
-        # Aggregate weights from multiple clients
-        averaged_weights = sum(weights) / len(weights)
+    def aggregate_weights(self, *encrypted_weights):
+        # Decrypt and aggregate weights from multiple clients
+        decrypted_weights = [encrypted_weight.get().decrypt() for encrypted_weight in encrypted_weights]
+        averaged_weights = sum(decrypted_weights) / len(decrypted_weights)
         self.model.load_state_dict(averaged_weights)
 
+        # Encrypt updated weights
+        encrypted_model_params = averaged_weights.encrypt(self.clients)
+
         # Send updated weights back to clients
-        for client in clients:
-            client.worker.send_updated_weights(self.model.state_dict())
+        for client in self.clients:
+            client.worker.send_updated_weights(encrypted_model_params)
 
 # Start a WebSocket server
 server = WebsocketServerWorker(host="0.0.0.0", port=8777, hook=hook, id="server")
